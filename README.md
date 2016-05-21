1705bdf470a932c2ce65a05240335ff67d66deea
rekire <git@reki.re>
2016-05-21 17:43:35 +0200

    Make it working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ParcelableUnitTest
-This is a small demo to show a problem with unit testing of a parcelable class.
+This is a small demo how to test Parcelable implementations. You might need to extend it to your
+needs it works as is for me. If you have any suggestions feel free to leave an issue.
+
+My test results a 100% coverage.
--- app/src/test/java/eu/rekisoft/android/parcelableproblem/MyParcelableTest.java
+++ app/src/test/java/eu/rekisoft/android/parcelableproblem/MyParcelableTest.java
@@ -5,56 +5,115 @@ import android.os.Parcel;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.powermock.api.mockito.PowerMockito.*;
-
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.CapturingMatcher;
+import org.mockito.internal.progress.HandyReturnValues;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.Serializable;
+import java.util.List;
+import java.util.Stack;
 import java.util.UUID;
 
 import de.mobilej.ABridge;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isNull;
+import static org.powermock.api.mockito.PowerMockito.doAnswer;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 /**
  * Created by rekire on 20.05.2016.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Parcel.class, ABridge.class})
 public class MyParcelableTest {
 
-    @Before
-    public void setup() {
-        mockStatic(ABridge.class);
-        when(ABridge.callBoolean(
-                eq("android.os.Parcel.nativeReadFloat(long)"),
-                isNull(), any(Object[].class))).thenAnswer(new Answer<Object>() {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable {
-                return 0f;
-            }
-        });
-    }
-
     @Test
-    public void demoCrash() {
+    public void parcelableVerification() {
         UUID id = UUID.randomUUID();
-        MyParcelable data = new MyParcelable("demo", 42, id, 0f);
+        MyParcelable data = new MyParcelable("demo", 42, id, 1f);
 
-        Parcel parcel = Parcel.obtain();
+        Parcel parcel = ParcelHelper.mockParcel();
         data.writeToParcel(parcel, 0);
+        //parcel.setDataPosition(0); // TODO mock this to reset the pos
 
-        // After you're done with writing, you need to reset the parcel for reading:
-        parcel.setDataPosition(0);
-
-        // Reconstruct object from parcel and asserts:
         MyParcelable sut = MyParcelable.CREATOR.createFromParcel(parcel);
         assertEquals("demo", sut.text);
         assertEquals(42, sut.count);
         assertEquals(id, sut.id);
-        assertEquals(0f, sut.problem, 0.01f);
+        assertEquals(1f, sut.problem, 0.01f);
+    }
+
+    @Test
+    public void coverageExtras() {
+        MyParcelable data = new MyParcelable("demo", 42, UUID.randomUUID(), 1f);
+        data.describeContents();
+        MyParcelable[] sut = MyParcelable.CREATOR.newArray(2);
+        assertNotNull(sut);
+        assertEquals(2, sut.length);
+    }
+
+    public static class ParcelHelper implements Answer<Object> {
+        private final HandyReturnValues handyReturnValues = new HandyReturnValues();
+        private final CapturingMatcher<Object> capturingMatcher = new CapturingMatcher<>();
+        private int pos = 0;
+
+        @Override
+        public Object answer(InvocationOnMock invocation) throws Throwable {
+            List<Object> stack = capturingMatcher.getAllValues();
+            Object value = stack.get(pos);
+            pos++;
+            return value;
+        }
+
+        public Integer captureInt() {
+            Mockito.argThat(capturingMatcher);
+            return handyReturnValues.returnFor(Integer.class);
+        }
+
+        public Float captureFloat() {
+            Mockito.argThat(capturingMatcher);
+            return handyReturnValues.returnFor(Float.class);
+        }
+
+        public String captureString() {
+            Mockito.argThat(capturingMatcher);
+            return handyReturnValues.returnFor(String.class);
+        }
+
+        public <T> T capture(Class<T> clazz) {
+            Mockito.argThat(capturingMatcher);
+            return handyReturnValues.returnFor(clazz);
+        }
+
+        public static Parcel mockParcel() {
+            Parcel parcel = Mockito.mock(Parcel.class);
+            ParcelHelper helper = new ParcelHelper();
+
+            // TODO there are much more calls which should been mocked
+
+            doNothing().when(parcel).writeString(helper.captureString());
+            doNothing().when(parcel).writeInt(helper.captureInt());
+            doNothing().when(parcel).writeSerializable(helper.capture(Serializable.class));
+            doNothing().when(parcel).writeFloat(helper.captureFloat());
+
+            when(parcel.readString()).then(helper);
+            when(parcel.readInt()).then(helper);
+            when(parcel.readSerializable()).then(helper);
+            when(parcel.readFloat()).then(helper);
+
+            return parcel;
+        }
     }
 }
